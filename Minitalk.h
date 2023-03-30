fb1d0de5d80fafe2de68e20a338129116ac88327
cj4ck <digital.k.jack@gmail.com>
2023-03-30 20:39:14 +0200

    Fix for my vs code

--- Minitalk.h
+++ Minitalk.h
@@ -6,12 +6,13 @@
 /*   By: cjackows <cjackows@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 11:46:40 by cjackows          #+#    #+#             */
-/*   Updated: 2023/03/30 10:11:27 by cjackows         ###   ########.fr       */
+/*   Updated: 2023/03/30 20:35:36 by cjackows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
+# define _XOPEN_SOURCE 700
 
 # include "libft/libft.h"
 # include <signal.h>
